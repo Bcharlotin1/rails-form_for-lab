b45629305b5bda3da89782dc2bc3e506db5c3764
bcharlotin1 <biancacharlotin1@gmail.com>
2021-07-04 09:24:20 -0400

    commit tree 9acd4e1d5a530b3cc55fa097c025bcfb0d8bac18

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students
+  resources :school_classes
+  # put 'school_classes/new', to: 'school_classes#new', as: "new_school_class"
+  # resources :school_classes, only: [:index, :show, :create, :edit, :update]
 end
